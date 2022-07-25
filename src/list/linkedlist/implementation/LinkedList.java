4dfd841838680aa60d0b6d1cbbbe0a3058a6df8a
DH <chaewon1130@naver.com>
2022-07-25 15:15:13 +0900

    LinkedList 구현

--- src/list/linkedlist/implementation/LinkedList.java
+++ src/list/linkedlist/implementation/LinkedList.java
@@ -1,24 +1,71 @@
 package list.linkedlist.implementation;
 
 public class LinkedList {
-    // 첫번째 노드를 가리키는 필드
-    private Node head;
-    private Node tail;
-    private int size = 0;
-    
-    
-    private class Node{
-        // 데이터가 저장될 필드
-        private Object data;
-        // 다음 노드를 가리키는 필드
-        private Node next;
-        public Node(Object input) {
-            this.data = input;
-            this.next = null;
-        }
-        // 노드의 내용을 쉽게 출력해서 확인해볼 수 있는 기능
-        public String toString(){
-            return String.valueOf(this.data);
-        }
-    }
+	private Node head;
+	private Node tail;
+	private int size = 0;
+	
+	private class Node{
+		private Object data;
+		private Node next;
+		public Node(Object input) {
+			this.data = input;
+			this.next = null;
+		}
+		public String toString() {
+			return String.valueOf(this.data);
+		}
+	}
+	
+	public void addFirst(Object input) {
+		Node newNode = new Node(input);
+		newNode.next = head;
+		head = newNode;
+		size++;
+		if(head.next == null) {
+			tail = head;
+		}
+	}
+	
+	public void addLast(Object input) {
+		Node newNode = new Node(input);
+		if(size == 0) {
+			addFirst(input);
+		}else {
+			tail.next = newNode;
+			tail = newNode;
+			size++;
+		}
+	}
+	
+	Node node(int index) {
+		Node x = head;
+		for(int i=0; i<index; i++) {
+			x = x.next;
+		}
+		return x;
+	}
+	
+	public void add(int k, Object input) {
+		if(k == 0) {
+			addFirst(input);
+		}else {
+			Node temp1 = node(k-1);
+			Node temp2 = temp1.next;
+			Node newNode = new Node(input);
+			temp1.next = newNode;
+			newNode.next = temp2;
+			size++;
+			if(newNode.next == null) {
+				tail = newNode;
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
 }
--- src/list/linkedlist/implementation/Main.java
+++ src/list/linkedlist/implementation/Main.java
@@ -2,7 +2,16 @@ package list.linkedlist.implementation;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList numbers = new LinkedList();
+    	LinkedList numbers = new LinkedList();
+//    	numbers.addFirst(30);
+//    	numbers.addFirst(20);
+//    	numbers.addFirst(10);
+    	
+    	numbers.addLast(10);
+    	numbers.addLast(20);
+    	numbers.addLast(30);  
+    	
+    	numbers.add(2, 25);
+    	
     }
- 
 }
